f793b46368dada192be9470f94f425cd65f7fa2f
Edwin Nkumama <edwinkitili@gmail.com>
2024-05-29 09:48:25 +0300

    Updated app component, module, and login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,29 @@
-// src/app/app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './components/login/login.component';
 import { KpiFormComponent } from './components/kpi-form/kpi-form.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { NgChartsModule } from 'ng2-charts';
 import { DashboardService } from './components/dashboard/dashboard.service';
-import {  MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import { MatInputModule} from '@angular/material/input';
-import { MatButtonModule} from '@angular/material/button';
-import { MatCardModule} from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { LayoutComponent } from './components/dashboard/layout/layout.component';
 import { SidenavComponent } from './components/dashboard/sidenav/sidenav.component';
 import { NavbarComponent } from './components/dashboard/navbar/navbar.component';
-import {MatToolbarModule} from '@angular/material/toolbar'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthenticationService } from './authentication.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     KpiFormComponent,
     DashboardComponent,
     LayoutComponent,
@@ -47,16 +45,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatCardModule,
     MatToolbarModule,
     BrowserAnimationsModule
-    
-    
-
-    
-    
-
-
-
   ],
-  providers: [DashboardService],
+  providers: [
+    DashboardService,
+    AuthenticationService,
+    provideHttpClient(withFetch())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
